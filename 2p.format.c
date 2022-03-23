0f5bd12afb594161a4a343b53292014c6f7bdae0
santiago <4547@holbertonschool.com>
2022-03-22 18:51:18 -0700

    funcion, libreria, formato

---
+++ 2p.format.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ * p_string - pring string
+ * @s: string to print
+ * Return: nothing
+ **/
+int p_string(char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_write(s[i]);
+	}
+	return (i);
+}
+/**
+ * buffer - Temporary string to hold all everything to print
+ * @a: character to add to string
+ * @s: pointer to place in current string
+ * Return: On success 1, 0 if string is NULL
+ **/
+int buffer(char a, char *s)
+{
+	if (s == NULL)
+		return (0);
+	s[0] = a;
+	return (1);
+}
+
+/**
+ * _write - writes the character c to stdout
+ * @c: the char to print
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _write(char c)
+{
+	return (write(1, &c, 1));
+}
--- best_print.c
+++ best_print.c
@@ -17,6 +17,7 @@ int best_print(char c, va_list *args)
 			{'s', print_string},
 			{'d', print_int},
 			{'i', print_int},
+			{'b', print_bin},
 			{'\0', NULL}
 	};
 	while (flags[i].letter)
